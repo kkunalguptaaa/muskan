3eaa7b0bf61684b18c3b4f077f717d729af83b59
Kunal gupta <kkunalguptaaa@gmail.com>
2020-09-06 02:42:53 +0530

    made appbar a differnet component

---
+++ src/components/navbar.js
@@ -0,0 +1,32 @@
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      display: 'flex',
+    },
+    paper: {
+      padding: theme.spacing(4),
+      margin: 'auto',
+      maxWidth: 900,
+    },
+    appBar: {
+      zIndex: theme.zIndex.drawer + 1,
+    }
+  }));
+export default  function Navbar(){
+    const classes = useStyles();
+    return(
+    <AppBar position="fixed" className={classes.appBar}>
+    <Toolbar>
+      <Typography variant="h6" noWrap>
+        Muskan
+      </Typography>
+    </Toolbar>
+  </AppBar>
+  );
+}
