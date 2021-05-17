7b2c670650df24b88ae1894f276145e9b4d2ee07
Kunal gupta <kkunalguptaaa@gmail.com>
2021-05-18 00:11:22 +0530

    Updates

---
+++ precache-manifest.89655adf3bb767c5bc700e24d9e61590.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56cd99abb068db36f007a5c22cae3194",
+    "url": "/muskan/index.html"
+  },
+  {
+    "revision": "3634c369cd95eca7815f",
+    "url": "/muskan/static/css/main.d3eddbfb.chunk.css"
+  },
+  {
+    "revision": "4e122fab90c9527f480d",
+    "url": "/muskan/static/js/2.12b2bab7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/muskan/static/js/2.12b2bab7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3634c369cd95eca7815f",
+    "url": "/muskan/static/js/main.2e77558a.chunk.js"
+  },
+  {
+    "revision": "3ff9c0383035329f7826",
+    "url": "/muskan/static/js/runtime-main.a2a3b8bb.js"
+  }
+]);
