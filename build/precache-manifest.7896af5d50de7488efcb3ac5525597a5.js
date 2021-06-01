2fd2ebe135a898983ade6be271b3ca09f9b6362d
Kunal gupta <kkunalguptaaa@gmail.com>
2021-06-02 01:16:08 +0530

    commit for bulid folder

--- build/precache-manifest.7896af5d50de7488efcb3ac5525597a5.js
+++ build/precache-manifest.7896af5d50de7488efcb3ac5525597a5.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2bd01622ac415de201ef414ff788f4dc",
+    "revision": "f70a887fba0f1ad9eebbfe2f4d917bdc",
     "url": "/muskan/index.html"
   },
   {
-    "revision": "d26820af3dcdc23ed889",
+    "revision": "d3e0e2ec0dfbb3111cc9",
     "url": "/muskan/static/css/main.d3eddbfb.chunk.css"
   },
   {
-    "revision": "d7580704a5ff4d013e70",
-    "url": "/muskan/static/js/2.31b0edad.chunk.js"
+    "revision": "21c216a51e9ac7807d98",
+    "url": "/muskan/static/js/2.fe620d38.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "/muskan/static/js/2.31b0edad.chunk.js.LICENSE.txt"
+    "url": "/muskan/static/js/2.fe620d38.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d26820af3dcdc23ed889",
-    "url": "/muskan/static/js/main.1b4dfc34.chunk.js"
+    "revision": "d3e0e2ec0dfbb3111cc9",
+    "url": "/muskan/static/js/main.c1056da9.chunk.js"
   },
   {
     "revision": "3ff9c0383035329f7826",
